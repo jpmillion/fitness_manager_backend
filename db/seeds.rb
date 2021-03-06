5641c3dfed81ebc427f9f0f2db2cbdbb5d798c61
john million <jpmillion@comcast.net>
2021-03-06 18:32:07 -0500

    created category model and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+speed_agility = Category.create(name: 'Speed and Agility Training', description: 'Speed is defined as the ability to move the body in one direction as fast as possible. Agility is the ability to accelerate, decelerate, stabilize, and quickly change directions with proper posture.')
+strength = Category.create(name: 'Strength Training', description: 'A system of physical conditioning in which muscles are exercised by being worked against an opposing force (as by lifting weights) to increase strength')
+plyometric = Category.create(name: 'Plyometric Training', description: 'Plyometrics is a type of exercise training that uses speed and force of different movements to build muscle power.')
