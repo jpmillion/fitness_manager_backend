dcc2124785b0b1bff677dfe3badf93b241e60444
john million <jpmillion@comcast.net>
2021-03-08 02:35:56 -0500

    index and create routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :workouts, only: :index
+      resources :workouts, only: [:index, :create]
     end
   end
 end
