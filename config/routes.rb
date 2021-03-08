30c24082ecd7aaeac56ac1d1e0e8ca209bc3002d
john million <jpmillion@comcast.net>
2021-03-08 02:13:34 -0500

    routes and controller for workouts

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,10 @@ Rails.application.routes.draw do
       resources :exercises, only: :index
     end
   end
+
+  namespace :api do
+    namespace :v1 do
+      resources :workouts, only: :index
+    end
+  end
 end
