e08e3de17acc5fc840ecb5f715cfba32eff5de25
john million <jpmillion@comcast.net>
2021-03-09 06:43:37 -0500

    exercise index action

--- app/controllers/api/v1/exercises_controller.rb
+++ app/controllers/api/v1/exercises_controller.rb
@@ -1,2 +1,8 @@
 class Api::V1::ExercisesController < ApplicationController
+    
+    def index
+        exercises = Exercise.all
+        options = { include: [:category, :workout] }
+        render json: ExerciseSerializer.new(exercises, options) 
+    end
 end
