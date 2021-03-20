f95e3334c3bb1231b648d0a88c288c08953c3230
john million <jpmillion@comcast.net>
2021-03-19 23:04:24 -0400

    added validation for name presence

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -3,5 +3,6 @@ class Workout < ApplicationRecord
     has_many :exercises, dependent: :nullify
     accepts_nested_attributes_for :exercises
 
+    validates :name, presence: true
     validates :name, uniqueness: { scope: :athlete }
 end
