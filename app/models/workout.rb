d9b292bdcb13bf36486a966c8f6b0e6213f622c8
john million <jpmillion@comcast.net>
2021-05-16 19:47:32 -0400

    commented out accepts nested attributes

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,7 +1,7 @@
 class Workout < ApplicationRecord
     belongs_to :athlete
     #has_many :exercises, dependent: :nullify
-    accepts_nested_attributes_for :exercises
+    #accepts_nested_attributes_for :exercises
 
     validates :name, presence: true
     validates :name, uniqueness: { scope: :athlete }
