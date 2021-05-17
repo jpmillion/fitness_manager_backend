3200bca3fc36ef2a5d806a2d07845d3e943eb6ce
john million <jpmillion@comcast.net>
2021-05-16 20:00:48 -0400

    uncommented exercise and workout relationship

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,6 +1,6 @@
 class Workout < ApplicationRecord
     belongs_to :athlete
-    #has_many :exercises, dependent: :nullify
+    has_many :exercises, dependent: :nullify
     #accepts_nested_attributes_for :exercises
 
     validates :name, presence: true
