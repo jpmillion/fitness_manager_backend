e48782064defef78c4b5d4b26f65f53137f9dc7f
john million <jpmillion@comcast.net>
2021-06-01 02:49:00 -0400

    included workouts with athlete on sessions create

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::SessionsController < ApplicationController
     athlete = Athlete.find_by(name: params[:name])
     if athlete && athlete.authenticate(params[:password])
         token = issue_token(athlete)
-        render json: { athlete: AthleteSerializer.new(athlete), token: token }
+        render json: { athlete: AthleteSerializer.new(athlete, { include: [:workouts] }), token: token }
     else
         render json: { errors: 'Invalid username or password', token: '' }
     end
